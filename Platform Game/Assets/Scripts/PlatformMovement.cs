ab28c51b4b112c020e5f2b935f83054b4eedbb48
Unknown <pyuli15@hotmail.com>
2017-03-07 15:04:42 -0500

    moving platforms and snow particles

---
+++ Platform Game/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+    //current position
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform childTransform;
+
+    [SerializeField]
+    private Transform transformB;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //current position of the platform
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        //where i want the platfom to go
+        nextPos = posB;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+
+    private void Move()
+    {
+        //need to take the transform of our small platform
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPos, speed * Time.deltaTime);
+
+        if (Vector3.Distance(childTransform.localPosition, nextPos) <= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+
+    private void ChangeDestination()
+    {
+        nextPos = nextPos != posA ? posA : posB;
+    }
+}
