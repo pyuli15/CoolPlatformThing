060e767ab900c3534d039f86942de186645ebeff
Pamela <pyuli15@hotmail.com>
2017-04-17 11:48:09 -0400

    edited animations

---
+++ Platform Game/Assets/Scripts/BirdsSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsSounds : MonoBehaviour {
+
+    public AudioClip[] animals;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayHitSound()
+    {
+        Sound.me.PlaySound(animals[Random.Range(0, animals.Length)], 1f);
+    }
+}
---
+++ Platform Game/Assets/Scripts/enemyDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDamage : MonoBehaviour {
+    public float damage;
+    public float damageRate;
+    //public float pushBackForce;
+
+    float nextDamage;
+
+    // Use this for initialization
+    void Start()
+    {
+        nextDamage = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player" && nextDamage < Time.time)
+        {
+            PlayerHealth thePlayerHealth = coll.gameObject.GetComponent<PlayerHealth>();
+            thePlayerHealth.addDamage(damage);
+            nextDamage = Time.time + damageRate;
+
+            //pushBack();
+
+        }
+    }
+
+}
+   
+
+
+
+
+
+
+
+
+
